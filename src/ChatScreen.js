b0fb8e8b7905461506d8e21eb8a865df99c4d539
JosephAwuku33 <33jaykay@gmail.com>
2023-10-23 23:16:22 +0000

    fixed formatting of math expressions

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { GiftedChat, InputToolbar } from 'react-native-gifted-chat';
-import { StyleSheet, ToastAndroid } from 'react-native';
+import { StyleSheet, ToastAndroid, View } from 'react-native';
 import { CHAT_GPT_API_KEY } from "@env";
 import { MathJaxSvg } from 'react-native-mathjax-html-to-svg';
-
 
 const ChatScreen = () => {
   const [messages, setMessages] = useState([]);
@@ -17,7 +16,7 @@ const ChatScreen = () => {
           messages: [
             {
               role: 'user',
-              content: message,
+              content: message + ". Please format the answer in LaTeX with the right delimiters ",
             },
           ],
           model: 'gpt-3.5-turbo',
@@ -38,15 +37,14 @@ const ChatScreen = () => {
   };
 
   const onSend = async (newMessages = []) => {
-    setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessages));
-    const response = await sendMessage(newMessages[0].text);
-    const latexString = `$${response}$`;
+    setMessages(prev => GiftedChat.append(prev, newMessages));
 
-    //console.log(latexString);
+    const response = await sendMessage(newMessages[0].text);
+    
     const chatMessage = [
       {
         _id: Math.random().toString(36).substring(7),
-        text: latexString,
+        text: response,
         createdAt: new Date(),
         user: {
           _id: 2,
@@ -59,6 +57,21 @@ const ChatScreen = () => {
     setMessages(prev => GiftedChat.append(prev, chatMessage));
   };
 
+  useEffect(() => {
+    const initialMessage = {
+      _id: Math.random().toString(36).substring(7),
+      text: "Welcome to the chat!, I'm Mathy, your personal Math homework assistant, enter any math problem, expression or equation you'd like for me to solve",
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: 'GPT-3.5-turbo',
+        avatar: require('../assets/favicon.png'),
+      },
+    };
+
+    setMessages([initialMessage]);
+  }, []);
+
   const user = {
     _id: 1,
     name: 'Developer',
@@ -69,16 +82,22 @@ const ChatScreen = () => {
     return <InputToolbar {...props} containerStyle={styles.input} />;
   };
 
-  const renderMessageText = props => {
-    return <MathJaxSvg fontSize={14} style={styles.MathContainer} fontCache={true}>
-
-      {props.currentMessage.text}
-    </MathJaxSvg>
-
-  };
+  const renderMessageText = (props) => {
+    return (
+      <MathJaxSvg 
+        fontSize={14}
+        color="#000000"
+        fontCache={true}
+        style={{padding: 10}}
+      >
+        {props.currentMessage.text}
+      </MathJaxSvg>
+    );
+  }
 
   return (
-    <GiftedChat messages={messages}
+    <GiftedChat 
+      messages={messages}
       onSend={onSend}
       user={user}
       placeholder={'Enter your expression'}
@@ -88,15 +107,13 @@ const ChatScreen = () => {
       messagesContainerStyle={styles.messageContainer}
       renderMessageText={renderMessageText}
     />
-
-  )
-
-    ;
+  );
 };
 
 const styles = StyleSheet.create({
   messageContainer: {
     paddingBottom: 30,
+    padding: 30,
     backgroundColor: "#E5E4EF",
   },
 
@@ -107,10 +124,8 @@ const styles = StyleSheet.create({
   input: {
     borderColor: 'transparent',
     borderWidth: 0,
-    padding: 5
+    
   },
 });
-
-
 
 export default ChatScreen;
